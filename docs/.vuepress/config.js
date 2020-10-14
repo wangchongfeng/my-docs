2744030ddd355b059d5721f51d2bab3eee617be9
wangchongfeng <m15011384464@163.com>
2020-10-14 17:13:46 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: 'Hello VuePress',
+  description: 'Just playing around',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'External', link: 'https://wancghonfeng.top' },
+    ],
+    sidebar: 'auto'
+  }
+}
